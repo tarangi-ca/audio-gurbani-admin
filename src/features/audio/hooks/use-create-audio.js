ad86349d2a674140ca8852b4069c5828f1d80a85
Anmol Brar <anmol@brars.net>
2025-01-19 16:01:17 +0000

    chore(features/audio/hooks/use-create-audio.js): deprecate old audio service, use new file domain (#13)
    Reviewed-on: https://git.brars.net/tarangi/audio-gurbani-admin/pulls/13
    Co-authored-by: Anmol Brar <anmol@brars.net>
    Co-committed-by: Anmol Brar <anmol@brars.net>

--- src/features/audio/hooks/use-create-audio.js
+++ src/features/audio/hooks/use-create-audio.js
@@ -1,24 +1,28 @@
 import { useMutation } from "@tanstack/react-query"
 import axios from "../../../utilities/axios"
 import { QUERY_KEY } from "../index"
+import { useCreateFile } from "../../file/hooks/use-create-file"
 
 export const useCreateAudio = () => {
+    const { mutate: createFile, data, isLoading, error } = useCreateFile()
+
     return useMutation({
         mutationFn: async ({ file, displayName, collectionId }) => {
-            const {
-                data: { id, url },
-            } = await axios.post(`/${QUERY_KEY}/pre-signed-url`)
+            createFile({ file, folder: "audios" })
 
-            await fetch(url, {
-                method: "PUT",
-                body: file,
-            })
+            if (!isLoading && !error) {
+                return await axios.post(
+                    `/${QUERY_KEY}/`,
+                    { id: data, displayName, collectionId },
+                    {
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                    }
+                )
+            }
 
-            return await axios.post(`/${QUERY_KEY}/`, {
-                id,
-                displayName,
-                collectionId,
-            })
+            return error
         },
     })
 }
